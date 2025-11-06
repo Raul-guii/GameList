0891db4093e806b38496063fba3586d0e67ac5a2
Raul Guilherme <raulawp460@gmail.com>
2025-11-06 11:25:37 -0300

    feat(frontend): implementado barra de pesquisa de jogos pelo nome

--- frontend/gamelist-frontend/src/app/app.component.ts
+++ frontend/gamelist-frontend/src/app/app.component.ts
@@ -2,14 +2,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { GameSearchComponent } from './components/game-search.component/game-search.component';
 import { GameListComponent } from './components/game-list/game-list.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, GameSearchComponent, GameListComponent, SidebarComponent],   
+  imports: [CommonModule, RouterModule, GameListComponent, SidebarComponent],   
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- frontend/gamelist-frontend/src/app/components/game-list/game-list.component.ts
+++ frontend/gamelist-frontend/src/app/components/game-list/game-list.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { GameDTO } from '../../models/game.model';
 import { GameService } from '../../services/game.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-game-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './game-list.component.html',
   styleUrl: './game-list.component.css'
 })
@@ -15,12 +16,13 @@ export class GameListComponent {
   games: GameDTO[] = [];
   loading = true;
   error?: string;
+  keyword: string = '';
 
-  constructor(private svc: GameService) {}
+  constructor(private gameService: GameService) {}
 
   load(): void {
     this.loading = true;
-    this.svc.getAll().subscribe({
+    this.gameService.getAll().subscribe({
       next: data => { this.games = data; this.loading = false; console.log('Games recebidos da API: ', this.games)},
       error: err => { this.error = 'Erro carregar jogos'; console.error(err); this.loading = false; }
     });
@@ -32,23 +34,32 @@ export class GameListComponent {
     img.onerror = null;
   }
 
-getHighResImage(url: string | undefined): string {
-  if (!url) return 'assets/placeholder.svg';
-  if (url.startsWith('//')) url = 'https:' + url;
-  if (url.startsWith('/')) url = 'https://media.rawg.io' + url;
-  if (url.startsWith('media.')) url = 'https://' + url;
-  if (url.includes('media.rawg.io')) {
-    url = url
-      .replace('/crop/200/', '/resize/640/')
-      .replace('/crop/300/', '/resize/640/')
-      .replace('/crop/400/', '/resize/800/');
-  }
+  getHighResImage(url: string | undefined): string {
+    if (!url) return 'assets/placeholder.svg';
+    if (url.startsWith('//')) url = 'https:' + url;
+    if (url.startsWith('/')) url = 'https://media.rawg.io' + url;
+    if (url.startsWith('media.')) url = 'https://' + url;
+    if (url.includes('media.rawg.io')) {
+      url = url
+        .replace('/crop/200/', '/resize/640/')
+        .replace('/crop/300/', '/resize/640/')
+        .replace('/crop/400/', '/resize/800/');
+    }
 
   if (url.includes('images.igdb.com')) {
     url = url.replace('t_thumb', 't_cover_big');
   }
 
   return url;
+  }
+
+    search(): void {
+    if (!this.keyword.trim()) return;
+
+    this.gameService.searchGames(this.keyword).subscribe({
+      next: (dados) => this.games = dados,
+      error: (erro) => console.error(erro)
+    })
   }
 
   ngOnInit(): void{
